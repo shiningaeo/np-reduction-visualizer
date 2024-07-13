6545f1749d4ba4e71a8d1f9892eb83df9c9d479a
Rinny Fan <rinnyfan@umich.edu>
2024-07-12 22:02:54 -0400

    Super basic of 3SAT -> VC SVG layout

--- src/app/animation.tsx
+++ src/app/animation.tsx
@@ -1,7 +1,7 @@
 import TriangleWidget from "./triangle-widget"
 
 export default function Animation({ index }) {
-    var test = [<TriangleWidget />,
+    var test = [<TriangleWidget x="0"/>,
     <svg width="60" height="60"><rect width="60" height="60" x="0" y="0" fill={'red'} /></svg>, 
     <svg width="60" height="60"><circle r="30" cx="30" cy="30" fill="red" /></svg>]
     
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,21 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <svg height="500" width="800">
-        <VariableWidget />
-        <TriangleWidget />
+        // difference of 144
+        <VariableWidget x="28"/>
+        <VariableWidget x="182"/>
+        <VariableWidget x="336"/>        
+        <VariableWidget x="490"/>
+        <VariableWidget x="644"/>
+
+        <TriangleWidget x="36"/>        
+        <TriangleWidget x="190"/>
+        <TriangleWidget x="344"/> 
+        <TriangleWidget x="498"/> 
+        <TriangleWidget x="652"/> 
       </svg>
       {/* <div className="z-7 w-full max-w-2xl items-center justify-between font-mono text-sm lg:flex"> */}
-        <Viewer />
+        {/* <Viewer /> */}
       {/* </div> */}
     </main>
   );
