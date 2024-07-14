2c1c91a13f50f6be7db4b48539e4fc0d08e6db18
Rinny Fan <rinnyfan@umich.edu>
2024-07-14 18:23:35 -0400

    Layout of 3sat->vc changes based on inputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ClauseGadget from './clause-gadget';
 import VariableGadget from './variable-gadget';
+import { mkdtemp } from 'fs';
 
 // 3SAT -> Vertex Cover (VC)
 
@@ -10,22 +11,30 @@ import VariableGadget from './variable-gadget';
 
 export default function Home() {
   var n = 5
-  var m = 3
+  var m = 5
   var input = [[1, 2, 3,], [4, 5, -1], [3, 1, -2], [4, -5, 2], [-1, 3, 4]]
-  
-  var visible_vars = ["visible", "visible", "visible", "visible", "visible"]
 
-  if (n == 3) { visible_vars = ["hidden", "visible", "visible", "visible", "hidden"]
-  } else if (n == 1) { visible_vars = ["hidden", "hidden", "visible", "hidden", "hidden"] }
+  var visible_vars = []
+  var visible_clauses = []
+  var offsets = [308, 231, 154, 77, 0]
+  var vars_offset = offsets[n-1]
+  var clause_offset = offsets[m-1]
+  
+  for (let i = 0; i < n; ++i) {
+    visible_vars.push("visible")
+  }
+  for (let i = n+1; i < 6; ++i) {
+    visible_vars.push("hidden")
+  }
+
+  for (let i = 0; i < m; ++i) {
+    visible_clauses.push("visible")
+  }
+  for (let i = m+1; i < 6; ++i) {
+    visible_clauses.push("hidden")
+  }
 
   const COLORS = ["blue", "red", "green", "orange", "purple"]
-  var gadget_colors = ["", "", "", "", ""]
-  var j = (n+1)%3
-
-  for (let i = 0; i < n; ++i) {
-    gadget_colors[j] = COLORS[i]
-    j++
-  } // for ..initialize colors of variable gadgets
 
   const [currIndex, setCurrIndex] = useState(0); // State to manage current step
   var steps = COLORS.length
@@ -36,18 +45,18 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between">
       <svg height="500" width="800">
-        // difference of 144
-        <VariableGadget x={28}  vis={visible_vars[0]} color={gadget_colors[0]}/>
-        <VariableGadget x={182} vis={visible_vars[1]} color={gadget_colors[1]}/>
-        <VariableGadget x={336} vis={visible_vars[2]} color={gadget_colors[2]}/>        
-        <VariableGadget x={490} vis={visible_vars[3]} color={gadget_colors[3]}/>
-        <VariableGadget x={644} vis={visible_vars[4]} color={gadget_colors[4]}/>
+        // difference of 154
+        <VariableGadget x={28+vars_offset}  vis={visible_vars[0]} color={COLORS[0]}/>
+        <VariableGadget x={182+vars_offset} vis={visible_vars[1]} color={COLORS[1]}/>
+        <VariableGadget x={336+vars_offset} vis={visible_vars[2]} color={COLORS[2]}/>        
+        <VariableGadget x={490+vars_offset} vis={visible_vars[3]} color={COLORS[3]}/>
+        <VariableGadget x={644+vars_offset} vis={visible_vars[4]} color={COLORS[4]}/>
 
-        <ClauseGadget x={36}  vis={visible_vars[0]}/>        
-        <ClauseGadget x={190} vis={visible_vars[1]}/>
-        <ClauseGadget x={344} vis={visible_vars[2]}/> 
-        <ClauseGadget x={498} vis={visible_vars[3]}/> 
-        <ClauseGadget x={652} vis={visible_vars[4]}/> 
+        <ClauseGadget x={36+clause_offset}  vis={visible_clauses[0]}/>        
+        <ClauseGadget x={190+clause_offset} vis={visible_clauses[1]}/>
+        <ClauseGadget x={344+clause_offset} vis={visible_clauses[2]}/> 
+        <ClauseGadget x={498+clause_offset} vis={visible_clauses[3]}/> 
+        <ClauseGadget x={652+clause_offset} vis={visible_clauses[4]}/> 
       </svg>
       <button onClick={handleClick} className="rounded-md border p-2 hover:bg-gray-100">
         <span>Next</span>
