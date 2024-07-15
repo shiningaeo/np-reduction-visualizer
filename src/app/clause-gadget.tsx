9368ce1f9caa016fc8d33adf0a5e333fec6b8c72
Rinny Fan <rinnyfan@umich.edu>
2024-07-15 00:34:49 -0400

    Color changes based on input

--- src/app/clause-gadget.tsx
+++ src/app/clause-gadget.tsx
@@ -1,4 +1,36 @@
-export default function ClauseGadget({x, vis}) {
+export default function ClauseGadget({x, vis, index, id1, id2, id3}) {
+    var color1 = "grey"
+    var color2 = "grey"
+    var color3 = "grey"
+
+    var stroke1 = 0
+    var stroke2 = 0
+    var stroke3 = 0
+
+    var border1 = ""
+    var border2 = ""
+    var border3 = ""
+
+    if (index[0] == id1) {
+        color1 = index[1]
+        if (index[1] != "") {
+            stroke1 = 3
+            border1 = index[2]
+        }
+    } else if (index[0] == id2) {
+        color2 = index[1]
+        if (index[1] != "") {
+            stroke2 = 3
+            border2 = index[2]
+        }
+    } else if (index[0] == id3) {
+        color3 = index[1]
+        if (index[1] != "") {
+            stroke3 = 3
+            border3 = index[2]
+        }
+    }
+
     return (
         <>
             <style jsx>{`
@@ -11,16 +43,11 @@ export default function ClauseGadget({x, vis}) {
                 <line x1="16" y1="112" x2="96" y2="112"  strokeWidth="3" stroke="black" />
                 <line x1="96" y1="112" x2="56" y2="32"  strokeWidth="3" stroke="black" />
                 <line x1="56" y1="32" x2="16" y2="112"  strokeWidth="3" stroke="black" />
-                <circle r="16" cx="16" cy="112" fill="grey"/>
-                <circle r="16" cx="96" cy="112" fill="grey"/>
-                <circle r="16" cx="56" cy="32" fill="grey"/>
+
+                <circle r="16" cx="16" cy="112" fill={color1} strokeWidth={stroke1} stroke={border1} />
+                <circle r="16" cx="56" cy="32" fill={color2} strokeWidth={stroke2} stroke={border2} />
+                <circle r="16" cx="96" cy="112" fill={color3} strokeWidth={stroke3} stroke={border3} />
             </svg>
         </>
     );
 }
-
-/* TODO: clause & widget, don't need to move, list of positions for connecting lines, draw it out
-
-/* line: <line x1="100" y1="0" x2="0" y2={Math.sqrt(3)*100}  strokeWidth="2" stroke="black" /> */
-/* circle: <circle r="45" cx="50" cy="50" fill="red" /> */
-/* rectangle: <rect width="200" height="100" x="10" y="10" rx="20" ry="20" fill={'red'} /> */
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,42 +2,61 @@
 import React, { useState, useEffect, useRef } from 'react';
 import ClauseGadget from './clause-gadget';
 import VariableGadget from './variable-gadget';
-import { mkdtemp } from 'fs';
 
 // 3SAT -> Vertex Cover (VC)
 
-// sample input: [[1, 2, 3,], [4, 5, -1], [3, 1, -2], [4, -5, 2], [-1, 3, 4]]
+// sample input: [1, 2, 3, 4, 5, -1, 3, 1, -2, 4, -5, 2, -1, 3, 4]
 //               5 variables, 3 clauses
 
 export default function Home() {
-  var n = 5
-  var m = 5
-  var input = [[1, 2, 3,], [4, 5, -1], [3, 1, -2], [4, -5, 2], [-1, 3, 4]]
+  const N = 5
+  const M = 5
+  const INPUT = [1, 2, 3, 4, 5, -1, 3, 1, -2, 4, -5, 2, -1, 3, 4]
 
-  var visible_vars = []
-  var visible_clauses = []
-  var offsets = [308, 231, 154, 77, 0]
-  var vars_offset = offsets[n-1]
-  var clause_offset = offsets[m-1]
-  
-  for (let i = 0; i < n; ++i) {
-    visible_vars.push("visible")
-  }
-  for (let i = n+1; i < 6; ++i) {
-    visible_vars.push("hidden")
-  }
-
-  for (let i = 0; i < m; ++i) {
-    visible_clauses.push("visible")
-  }
-  for (let i = m+1; i < 6; ++i) {
-    visible_clauses.push("hidden")
-  }
+  const OFFSETS = [308, 231, 154, 77, 0]
+  const VAR_OFFSETS = OFFSETS[N-1]
+  const CLAUSE_OFFSETS = OFFSETS[M-1]
 
   const COLORS = ["blue", "red", "green", "orange", "purple"]
 
+  var vis_vars = []
+  var vis_clauses = []
+
+  // [<id>, <color>, <border-color>]
+  var sequence = [
+    ["a1", "", ""], ["a2", "", ""], ["a3", "", ""],
+    ["b1", "", ""], ["b2", "", ""], ["b3", "", ""],
+    ["c1", "", ""], ["c2", "", ""], ["c3", "", ""],
+    ["d1", "", ""], ["d2", "", ""], ["d3", "", ""],
+    ["e1", "", ""], ["e2", "", ""], ["e3", "", ""],
+  ]
+
+  // initialize sequence based on input
+  for (let i = 0; i < INPUT.length; ++i) {
+    sequence[i][1] = COLORS[Math.abs(INPUT[i])-1]
+    if (INPUT[i] < 0) {
+      sequence[i][2] = "red"
+    }
+  } // for ..input size
+  
+  // layout of variable widgets
+  for (let i = 0; i < N; ++i) {
+    vis_vars.push("visible")
+  }
+  for (let i = N+1; i < 6; ++i) {
+    vis_vars.push("hidden")
+  }
+
+  // layout of clause widgets
+  for (let i = 0; i < M; ++i) {
+    vis_vars.push("visible")
+  }
+  for (let i = M+1; i < 6; ++i) {
+    vis_vars.push("hidden")
+  }
+
   const [currIndex, setCurrIndex] = useState(0); // State to manage current step
-  var steps = COLORS.length
+  var steps = sequence.length
   function handleClick() {
       setCurrIndex((prevStep) => (prevStep + 1) % steps);
   }
@@ -46,17 +65,17 @@ export default function Home() {
     <main className="flex flex-col items-center justify-between">
       <svg height="500" width="800">
         // difference of 154
-        <VariableGadget x={28+vars_offset}  vis={visible_vars[0]} color={COLORS[0]}/>
-        <VariableGadget x={182+vars_offset} vis={visible_vars[1]} color={COLORS[1]}/>
-        <VariableGadget x={336+vars_offset} vis={visible_vars[2]} color={COLORS[2]}/>        
-        <VariableGadget x={490+vars_offset} vis={visible_vars[3]} color={COLORS[3]}/>
-        <VariableGadget x={644+vars_offset} vis={visible_vars[4]} color={COLORS[4]}/>
+        <VariableGadget x={28+VAR_OFFSETS}  vis={vis_vars[0]} color={COLORS[0]}/>
+        <VariableGadget x={182+VAR_OFFSETS} vis={vis_vars[1]} color={COLORS[1]}/>
+        <VariableGadget x={336+VAR_OFFSETS} vis={vis_vars[2]} color={COLORS[2]}/>        
+        <VariableGadget x={490+VAR_OFFSETS} vis={vis_vars[3]} color={COLORS[3]}/>
+        <VariableGadget x={644+VAR_OFFSETS} vis={vis_vars[4]} color={COLORS[4]}/>
 
-        <ClauseGadget x={36+clause_offset}  vis={visible_clauses[0]}/>        
-        <ClauseGadget x={190+clause_offset} vis={visible_clauses[1]}/>
-        <ClauseGadget x={344+clause_offset} vis={visible_clauses[2]}/> 
-        <ClauseGadget x={498+clause_offset} vis={visible_clauses[3]}/> 
-        <ClauseGadget x={652+clause_offset} vis={visible_clauses[4]}/> 
+        <ClauseGadget x={36+CLAUSE_OFFSETS}  vis={vis_clauses[0]} index={sequence[currIndex]} id1="a1" id2="a2" id3="a3"/>        
+        <ClauseGadget x={190+CLAUSE_OFFSETS} vis={vis_clauses[1]} index={sequence[currIndex]} id1="b1" id2="b2" id3="b3"/>
+        <ClauseGadget x={344+CLAUSE_OFFSETS} vis={vis_clauses[2]} index={sequence[currIndex]} id1="c1" id2="c2" id3="c3"/> 
+        <ClauseGadget x={498+CLAUSE_OFFSETS} vis={vis_clauses[3]} index={sequence[currIndex]} id1="d1" id2="d2" id3="d3"/> 
+        <ClauseGadget x={652+CLAUSE_OFFSETS} vis={vis_clauses[4]} index={sequence[currIndex]} id1="e1" id2="e2" id3="e3"/> 
       </svg>
       <button onClick={handleClick} className="rounded-md border p-2 hover:bg-gray-100">
         <span>Next</span>
