043e8da49f1e73ec77df62ac8f687b6ce922cefb
Rinny Fan <rinnyfan@umich.edu>
2024-07-16 16:12:21 -0400

    More serious code cleanup

--- src/app/3sat-to-vc.tsx
+++ src/app/3sat-to-vc.tsx
@@ -1,13 +1,10 @@
 'use client'
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState } from 'react';
 import ClauseGadget from './clause-gadget';
 import VariableGadget from './variable-gadget';
 import Edge from './edge';
 
 // 3SAT -> Vertex Cover (VC)
-
-// sample input: [1, 2, 3, 4, 5, -1, 3, 1, -2, 4, -5, 2, -1, -3, 4]
-//               5 variables, 5 clauses
 
 export default function Three_SAT_VC() {
   const N = 5
@@ -20,52 +17,49 @@ export default function Three_SAT_VC() {
   const CLAUSE_OFFSETS = OFFSETS[M-1]
 
   const COLORS = ["blue", "red", "green", "orange", "purple"]
-  const COLOR_GROUPS = [["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""]]
-  const BORDER_GROUPS = [["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""], ["", "", ""]]
+  const COLOR_GROUPS = []
+  const BORDER_GROUPS = []
 
-  const TRUE_VAR_SPOTS = [44, 198, 352, 506, 660]
+  const TRUE_VAR_SPOTS = [44, 198, 352, 506, 660] // difference of 154
   const FALSE_VAR_SPOTS = [140, 294, 448, 602, 756]
-  var VAR_SPOTS = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0]
+  var VAR_SPOTS = []
 
   // initialize COLOR_GROUPS to preserve color changes
-  var track = 0
   for (let i = 0; i < INPUT.length; i+=3) {
     var temp = [COLORS[Math.abs(INPUT[i])-1], COLORS[Math.abs(INPUT[i+1])-1], COLORS[Math.abs(INPUT[i+2])-1]]
-    COLOR_GROUPS[track] = temp
-    ++track
+    COLOR_GROUPS.push(temp)
   }
 
   // initialize BORDER_GROUPS to preserve border changes
-  var track1 = 0
+  var track = 0
   for (let i = 0; i < INPUT.length; i+=3) {
-    var temp1 = ["", "", ""]
+    let temp1 = ["", "", ""]
     for (let j = 0; j < 3; ++j) {
       if (INPUT[i+j] < 0) {
           temp1[j] = "black"
       } 
     }
-
-    BORDER_GROUPS[track1] = temp1
-    ++track1
+    BORDER_GROUPS.push(temp1)
+    ++track
   }
 
   // initialize VAR_SPOTS for drawing edges
   for (let i = 0; i < INPUT.length; ++i) {
     if (INPUT[i] < 0) {
-      VAR_SPOTS[i] = FALSE_VAR_SPOTS[-1*INPUT[i]-1]+VAR_OFFSETS
+      VAR_SPOTS.push(FALSE_VAR_SPOTS[-1*INPUT[i]-1]+VAR_OFFSETS)
     } else {
-      VAR_SPOTS[i] = TRUE_VAR_SPOTS[INPUT[i]-1]+VAR_OFFSETS
+      VAR_SPOTS.push(TRUE_VAR_SPOTS[INPUT[i]-1]+VAR_OFFSETS)
     }
   }
 
-  var letters = ["a", "b", "c", "d", "e"]
-  var sequence = []
-  var track2 = 0
+  const LETTERS = ["a", "b", "c", "d", "e"]
+  var sequence = [] // IMPORTANT: walkthrough sequence
+  track = 0
   for (let i = 0; i < INPUT.length; i+=3) {
     for (let j = 1; j < 4; ++j) {
-      sequence.push(letters[track2].concat(j.toString()))
+      sequence.push(LETTERS[track].concat(j.toString()))
     }
-    ++track2
+    ++track
   }
 
   const [currIndex, setCurrIndex] = useState(-1); // State to manage current step
@@ -75,39 +69,37 @@ export default function Three_SAT_VC() {
   }
 
   var variables = []
-  var track3 = 28
+  track = 28
   for (let i = 0; i < N; ++i) {
-    variables.push(<VariableGadget key={i} x={track3 + VAR_OFFSETS} color={COLORS[i]}/>)
-    track3 += 154
+    variables.push(<VariableGadget key={i} x={track + VAR_OFFSETS} color={COLORS[i]}/>)
+    track += 154
   }
 
   var clauses = []
-  var track4 = 36
+  track = 36
   for (let i = 0; i < M; ++i) {
-    clauses.push(<ClauseGadget key={i} x={track4+CLAUSE_OFFSETS} index={sequence[currIndex]} 
-      id1={letters[i]+"1"} id2={letters[i]+"2"} id3={letters[i]+"3"} colors={COLOR_GROUPS[i]} borders={BORDER_GROUPS[i]}/> )
-    track4 += 154
+    clauses.push(<ClauseGadget key={i} x={track+CLAUSE_OFFSETS} index={sequence[currIndex]} 
+      id1={LETTERS[i]+"1"} id2={LETTERS[i]+"2"} id3={LETTERS[i]+"3"} colors={COLOR_GROUPS[i]} borders={BORDER_GROUPS[i]}/> )
+    track += 154
   }
 
   var edges = []
-  var track6 = 54
+  track = 54
   for (let i = 0; i < M; ++i) {
-    edges.push(<Edge key={i%3} index={sequence[currIndex]} id={letters[i]+"1"} x1={VAR_SPOTS[i%3]} x2={track6+CLAUSE_OFFSETS} y2={362}/>)
-    edges.push(<Edge key={i%3+1} index={sequence[currIndex]} id={letters[i]+"2"} x1={VAR_SPOTS[i%3 + 1]} x2={track6+40+CLAUSE_OFFSETS} y2={282}/>)
-    edges.push(<Edge key={i%3+2} index={sequence[currIndex]} id={letters[i]+"3"} x1={VAR_SPOTS[i%3 + 2]} x2={track6+80+CLAUSE_OFFSETS} y2={362}/>)
-    track6 += 154
+    edges.push(<Edge key={i%3} index={sequence[currIndex]} id={LETTERS[i]+"1"} x1={VAR_SPOTS[i%3]} x2={track+CLAUSE_OFFSETS} y2={362}/>)
+    edges.push(<Edge key={i%3+1} index={sequence[currIndex]} id={LETTERS[i]+"2"} x1={VAR_SPOTS[i%3 + 1]} x2={track+40+CLAUSE_OFFSETS} y2={282}/>)
+    edges.push(<Edge key={i%3+2} index={sequence[currIndex]} id={LETTERS[i]+"3"} x1={VAR_SPOTS[i%3 + 2]} x2={track+80+CLAUSE_OFFSETS} y2={362}/>)
+    track += 154
   }
 
   return (
     <main className="flex flex-col items-center justify-between">
       <svg height="500" width="800">
-        // difference of 154
-
-        {edges.map(y => y)}
+        {edges.map(edge => edge)}
         
-        {variables.map(component => component)}
+        {variables.map(v => v)}
 
-        {clauses.map(x => x)}
+        {clauses.map(clause => clause)}
       </svg>
       <button onClick={handleClick} className="rounded-md border p-2 hover:bg-gray-100">
         <span>Next</span>
