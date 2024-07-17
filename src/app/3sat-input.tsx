2d6016592b019b0689fae7fd7a89e8b76a8fe614
Rinny Fan <rinnyfan@umich.edu>
2024-07-17 12:41:12 -0400

    3SAT input layout adjustments

--- src/app/3sat-input.tsx
+++ src/app/3sat-input.tsx
@@ -8,30 +8,30 @@ export default function Three_SAT_Input({m}) {
     var render = []
     for (let i = 0; i < M-1; ++i) {
         render.push(<Clause key={i}/>)
-        render.push(<p key={i*-1} style={{fontSize: 35, margin: -15}}>&#8896;</p>)
+        render.push(<p key={(i+1)*-1} style={{fontSize: 35, margin: -15}}>&#8896;</p>)
     }
     render.push(<Clause key={m-1}/>)
     return (
-    <main className="flex flex-row items-start" style={{marginTop: 20}}>
-        <div>
-            <div style={{height: 40}}></div>
-            <label style={{marginTop: 20, fontSize: 30, fontWeight: 550}}> N:&nbsp;
+    <main className="flex flex-row justify-center" style={{marginTop: 20, width: 700, height: 350}}>
+        <div style={{minWidth: 200}} className="items-center">
+            <div style={{height: 155}}></div>
+            <label style={{fontSize: 30, fontWeight: 550}}> N:&nbsp;
                 <input value={N} onChange={e => setN(parseInt(e.target.value))} name="n"
                         min="1" max="5" style={{fontWeight: "normal"}} type="number"/>
             </label>
 
-            <label style={{marginTop: 20, fontSize: 30, fontWeight: 550}}> M:&nbsp;
+            <label style={{fontSize: 30, fontWeight: 550}}> M:&nbsp;
                 <input value={M} onChange={e => setM(parseInt(e.target.value))} name="m"
                         min="1" max="5" style={{fontWeight: "normal"}} type="number"/>
             </label>
         </div>
         <div style={{width: 50}}></div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center justify-center">
             {render.map(component => component)}
         </div>
         <div style={{width: 100}}></div>
         <div>
-            <button style={{marginTop: 180, marginLeft: -30}} 
+            <button style={{marginTop: 160, marginLeft: -30}} 
                 className="bg-gray-100 rounded-md border p-2 hover:bg-emerald-400">
                 <span>SUBMIT</span>
             </button>
--- src/app/clause-input.tsx
+++ src/app/clause-input.tsx
@@ -1,21 +1,19 @@
 export default function Clause() {
     return (
       <>
-        <div className="w-full h-1/2 items-center flex flex-col justify-center">
-            <div>
-                <p style={{display: "inline", fontSize: 30, fontWeight: 550}}>( </p>
-                <label style={{fontSize: 30, fontWeight: 550}}>X </label>
-                <input type="number" min="1" max="5" id="pin" name="pin" maxLength={1} size={1}></input>
-                <p style={{display: "inline", fontSize: 35}}> &#8897; </p>
+        <div>
+            <p style={{display: "inline", fontSize: 30, fontWeight: 550}}>( </p>
+            <label style={{fontSize: 30, fontWeight: 550}}>X </label>
+            <input type="number" min="1" max="5" id="pin" name="pin"></input>
+            <p style={{display: "inline", fontSize: 35}}> &#8897; </p>
 
-                <label style={{fontSize: 30, fontWeight: 550}}> X </label>
-                <input type="number"  min="1" max="5" id="pin" name="pin" maxLength={1} size={1}></input>
-                <p style={{display: "inline", fontSize: 35}}> &#8897;</p>
+            <label style={{fontSize: 30, fontWeight: 550}}> X </label>
+            <input type="number"  min="1" max="5" id="pin" name="pin" ></input>
+            <p style={{display: "inline", fontSize: 35}}> &#8897;</p>
 
-                <label style={{fontSize: 30, fontWeight: 550}}> X </label>
-                <input type="number"  min="1" max="5" id="pin" name="pin" maxLength={1} size={1}></input>
-                <p style={{display: "inline", fontSize: 30, fontWeight: 550}}> )</p>
-            </div>
+            <label style={{fontSize: 30, fontWeight: 550}}> X </label>
+            <input type="number"  min="1" max="5" id="pin" name="pin"></input>
+            <p style={{display: "inline", fontSize: 30, fontWeight: 550}}> )</p>
         </div>
       </>  
     );
