1f71a1f47a4e52f7e99f9e25eb63f264d84433df
Rinny Fan <rinnyfan@umich.edu>
2024-07-21 19:41:47 -0400

    Generate button, new view loads

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,56 +56,57 @@ export default function Home() {
 
   return (
     <>
-    <ProblemMenu />
-    <main className="flex flex-col items-center justify-between" style={{marginTop:10}}>
-      <div className="flex flex-row justify-center items-start w-full h-full">
-        {/* left half of problem selection */}
-        <div className="flex flex-row w-1/2" style={{borderRight: "solid 2px gray"}}>
-          <div className="flex flex-col w-1/12"></div>
-          <div className="flex flex-col w-11/12 h-full justify-center items-center" style={{backgroundColor:"#ffffff"}}>
-            <div className="w-full" style={{height:100, zIndex:10, backgroundColor:"#ffffff", padding:10}}>
-              <p>An instance of 3-SAT is comprised of <strong>n</strong> variables, <strong>m</strong> clauses.
-                Each clause contains 3 variables joined by &#8897; , the "or" operator. The clauses are joined by
-                &#8896; , the "and" operator. A valid instance of 3-SAT contains an assignment of variables such that
-                all the clauses evaluate to true.
-              </p>
+    {!submit ? (
+      <>
+      <ProblemMenu />
+      <main className="flex flex-col items-center justify-between" style={{marginTop:10}}>
+        <div className="flex flex-row justify-center items-start w-full h-full">
+          {/* left half of problem selection */}
+          <div className="flex flex-row w-1/2" style={{borderRight: "solid 2px gray"}}>
+            <div className="flex flex-col w-1/12"></div>
+            <div className="flex flex-col w-11/12 h-full justify-center items-center" style={{backgroundColor:"#ffffff"}}>
+              <div className="w-full" style={{height:100, zIndex:10, backgroundColor:"#ffffff", padding:10}}>
+                <p>An instance of 3-SAT is comprised of <strong>n</strong> variables, <strong>m</strong> clauses.
+                  Each clause contains 3 variables joined by &#8897; , the "or" operator. The clauses are joined by
+                  &#8896; , the "and" operator. A valid instance of 3-SAT contains an assignment of variables such that
+                  all the clauses evaluate to true.
+                </p>
+              </div>
+                  <Three_SAT_Input
+                    N={N}
+                    M={M}
+                    negatives={negatives}
+                    handleChangeN={handleChangeN}
+                    handleChangeM={handleChangeM}
+                    toggleNegation={toggleNegation}
+                    changeInput={changeInput}
+                  />
+                
             </div>
-            {!submit ? (
-              <>
-                <Three_SAT_Input
-                  N={N}
-                  M={M}
-                  negatives={negatives}
-                  handleChangeN={handleChangeN}
-                  handleChangeM={handleChangeM}
-                  toggleNegation={toggleNegation}
-                  changeInput={changeInput}
-                />
-              </>
-            ) : (
-              <Three_SAT_VC N={N} M={M} INPUT={input} />
-            )}
           </div>
-        </div>
 
-        <div className="flex flex-row w-1/2 h-full">
-          <div className="flex flex-col w-11/12 h-full justify-center" style={{backgroundColor:"#ffffff"}}>
-            <div className="w-full h-full" style={{height: 116, zIndex:10, backgroundColor:"#ffffff", padding:10}}>
-              <p>A vertex cover instance is a graph with <strong>V</strong> vertices, <strong>E</strong> edges, and
-                a budget of size <strong>k</strong>. A valid vertex cover is a subset of the vertices such that every
-                edge has at least one endpoint in the subset. The vertex cover problem is typically from the perspective
-                of the minimum cover size. In other words, a valid instance of vertex cover should have a size of at least k.
-              </p>
+          <div className="flex flex-row w-1/2 h-full">
+            <div className="flex flex-col w-11/12 h-full justify-center" style={{backgroundColor:"#ffffff"}}>
+              <div className="w-full h-full" style={{height: 116, zIndex:10, backgroundColor:"#ffffff", padding:10}}>
+                <p>A vertex cover instance is a graph with <strong>V</strong> vertices, <strong>E</strong> edges, and
+                  a budget of size <strong>k</strong>. A valid vertex cover is a subset of the vertices such that every
+                  edge has at least one endpoint in the subset. The vertex cover problem is typically from the perspective
+                  of the minimum cover size. In other words, a valid instance of vertex cover should have a size of at least k.
+                </p>
+              </div>
+              <div className="w-full" style={{height:306, backgroundColor:"#ffffff"}}></div>
+              <div className="w-full flex flex-row justify-center items-center" style={{height:130, backgroundColor:"white"}}>
+                <button onClick={handleSubmit} style={{zIndex:1, backgroundColor:"#b6f0e7", padding:15}}>GENERATE</button>
+              </div>
             </div>
-            <div className="w-full" style={{height:306, backgroundColor:"#ffffff"}}></div>
-            <div className="w-full flex flex-row justify-center items-center" style={{height:130, backgroundColor:"white"}}>
-              <button onClick={handleSubmit} style={{zIndex:1, backgroundColor:"#b6f0e7", padding:15}}>GENERATE</button>
-            </div>
+            <div className="w-1/12"></div>
           </div>
-          <div className="w-1/12"></div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
+    ) : (
+      <Three_SAT_VC N={N} M={M} INPUT={input} />
+    )}
     </>
   );
 }
