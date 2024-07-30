0ba497b130015a66358e351f43d71e9348cdad9f
Rinny Fan <rinnyfan@umich.edu>
2024-07-29 21:03:13 -0400

    NP description

--- src/app/components/descriptions/NPDetails.tsx
+++ src/app/components/descriptions/NPDetails.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
 
 export default function NPDetails() {
     return (
     <>
         <div className="w-full h-full bg-white p-5">
-            <h1 className="text-2xl font-bold mb-4">A Brief Intro to P vs. NP and their Significance</h1>
+            <h1 className="text-2xl font-bold mb-4">A Brief Intro to P vs. NP and the Significance</h1>
             
             <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div>
@@ -25,7 +26,24 @@ export default function NPDetails() {
                         <br></br>
                         We still don't know the answer.
                     </p>
-
+                    <div className="w-full flex items-center justify-center">
+                        <Image
+                            src="/p-np.svg"
+                            width={400}
+                            height={300}
+                            className="hidden md:block"
+                            alt="Screenshots of the dashboard project showing desktop version"
+                        />
+                    </div>
+                    <div>
+                        <h2 className="text-xl font-semibold mb-2">Significance and Open Questions</h2>
+                        <p className="mb-2">
+                            NP problems are significant in computer science because they include many problems that are critical in various fields such as cryptography, scheduling, and optimization. The relationship between NP problems and another class called P (problems that can be solved in polynomial time) is a central question in computational complexity theory.
+                        </p>
+                        <p className="mb-2">
+                            The famous open question, "Does P equal NP?" asks whether every problem for which a solution can be quickly verified can also be quickly solved. <strong>This question remains one of the most important unsolved problems in computer science.</strong>
+                        </p>
+                    </div>
                 </div>
                 
                 <div>
@@ -37,24 +55,26 @@ export default function NPDetails() {
                         <strong>Verification:</strong> The process of checking whether a given solution to a decision problem results in a 
                         "yes" result or a "no" result.
                     </p>
-                </div>
-                <div>
-                    <h2 className="text-xl font-semibold mb-2">Significance and Open Questions</h2>
-                    <p className="mb-2">
-                        NP problems are significant in computer science because they include many problems that are critical in various fields such as cryptography, scheduling, and optimization. The relationship between NP problems and another class called P (problems that can be solved in polynomial time) is a central question in computational complexity theory.
-                    </p>
-                    <p className="mb-2">
-                        The famous open question, "Does P equal NP?" asks whether every problem for which a solution can be quickly verified can also be quickly solved. This question remains one of the most important unsolved problems in computer science.
-                    </p>
-                    <img src="path/to/image4.jpg" alt="P vs NP Question" className="w-full h-auto mb-4"/>
-                </div>
-
-                <div>
-                    <h2 className="text-xl font-semibold mb-2">NP Problem Example: Traveling Salesman Problem</h2>
-                    <p className="mb-2">
-                        A classic example of an NP problem is the "Traveling Salesman Problem" (TSP). The problem asks whether a salesman can visit a set of cities, each only once, and return to the starting city with a total travel distance less than or equal to a given number. If someone provides a specific route, checking the total distance and verifying if it's within the limit can be done quickly. However, finding the optimal route among many cities is computationally challenging and no polynomial-time algorithm is known for solving it.
-                    </p>
-                    <img src="path/to/image3.jpg" alt="Traveling Salesman Problem" className="w-full h-auto mb-4"/>
+                    <div className="mt-8">
+                        <h2 className="text-xl font-semibold mb-2">Polynomial-Time Mapping Reductions</h2>
+                        <p className="mb-2">
+                           One method used in proving that a prolem is in NP is to construct a method of mapping instances
+                           of one NP problem to another. The reduction is successful if we can always map an arbitrary yes instance
+                           of one problem to a yes instance of the problem we're trying to prove is in NP, and likewise for false
+                           instances.
+                           <br /><br />
+                           For example, the reduction from 3SAT to vertex cover could be depicted as:
+                        </p>
+                        <div className="flex items-center justify-center mt-8">
+                            <Image
+                                src="/mapping.svg"
+                                width={500}
+                                height={300}
+                                className="hidden md:block"
+                                alt="Screenshots of the dashboard project showing desktop version"
+                            />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
