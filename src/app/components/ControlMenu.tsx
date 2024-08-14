8ea65d3897c994ec109acc8aa5cd27b5c54f80db
RF <rinnyfan@umich.edu>
2024-08-13 14:48:44 -1000

    Separated walkthru control menu as component

---
+++ src/app/components/ControlMenu.tsx
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+export default function ControlMenu({ currIndex, setCurrIndex, sequence, skipIdx }) {
+    const [nextColor, setNextColor] = useState("green")
+    const [backColor, setBackColor] = useState("gray")
+    const [skipColor, setSkipColor] = useState("coral")
+    const [skipBackColor, setSkipBackColor] = useState("gray")
+
+    let steps = sequence.length
+    function handleClick() {
+        setCurrIndex((prevStep) => {
+        let newIndex = prevStep + 1;
+        if (newIndex > steps - 1) {
+            newIndex = newIndex - 1
+        } else if (newIndex == steps - 1) {
+            setNextColor("gray")
+            setSkipColor("gray")
+        }
+        if (newIndex > 0) {
+            setBackColor("green")
+            setSkipBackColor("coral")
+        }
+        return newIndex;
+        });
+    }
+
+    function handleClickBack() {
+        setCurrIndex((prevStep) => {
+        let newIndex = prevStep - 1;
+        if (newIndex < 0) {
+            newIndex = 0
+        } else if (newIndex == 0) {
+            setBackColor("gray")
+            setSkipBackColor("gray")
+        }
+        if (newIndex < steps-1) {
+            setNextColor("green")
+            setSkipColor("coral")
+        }
+        return newIndex;
+        });
+    }
+
+    function handleSkipBack() {
+        // be careful when doing logic for other proofs ...
+        if (sequence[currIndex][0] < "g" && sequence[currIndex][0] >= "b") {
+          setCurrIndex(3)
+        } else {
+          handleClickBack()
+        }
+      }
+
+    function handleSkipFront() {
+        if (currIndex == sequence.length-1) {
+            return null
+        }
+        // be careful when doing logic for other proofs ...
+        if (sequence[currIndex] >= "a3" && sequence[currIndex+1][0] < "g") {
+            setCurrIndex(3+skipIdx*3)
+        } else {
+            handleClick()
+        }
+    }
+    
+
+    return (
+    <>
+        <div className="flex flex-row w-3/12 justify-center items-center p-5" style={{marginTop:180, backgroundColor:"#EAF1F2", height:70, borderRadius:10}}>
+          <div className="flex flex-col items-center justify-center w-1/4">
+            <button onClick={handleSkipBack}>
+                <span style={{color:skipBackColor, fontSize:24}}>&#9664;&#9664;</span>
+            </button>
+            <p style={{fontSize:12}}>skip back</p>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/4">
+            <button onClick={handleClickBack}>
+              <span style={{color:backColor, fontSize:24}}>&#9664;</span>
+            </button>
+            <p style={{fontSize:12}}>back</p>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/4">
+            <button onClick={handleClick}>
+              <span style={{color:nextColor, fontSize:24}}>&#9654;</span>
+            </button>
+            <p style={{fontSize:12}}>next</p>
+          </div>
+          <div className="flex flex-col items-center justify-center w-1/4">
+            <button onClick={handleSkipFront}>
+              <span style={{color:skipColor, fontSize:24}}>&#9654;&#9654;</span>
+            </button>
+            <p style={{fontSize:12}}>skip forward</p>
+          </div>
+      </div>
+    </>
+    );
+}
--- src/app/components/sat-to-vc/Sat-VC.tsx
+++ src/app/components/sat-to-vc/Sat-VC.tsx
@@ -6,6 +6,7 @@ import Edge from './Edge';
 import { poppins } from '../../fonts/fonts';
 import OrangeBox from './OrangeBox';
 import ContentBox from './ContentBox';
+import ControlMenu from '../ControlMenu';
 
 // 3SAT -> Vertex Cover (VC)
 
@@ -35,66 +36,8 @@ export default function Three_SAT_VC({N, M, INPUT, setSubmit, setSubmit2}) {
   }
   sequence.push.apply(sequence, ["g1", "g2", "g3", "g4"])
 
-  // IMPORTANT STATE VARIABLE
-  const [currIndex, setCurrIndex] = useState(0) // State0 to manage current step
-  const [nextColor, setNextColor] = useState("green")
-  const [backColor, setBackColor] = useState("gray")
-  const [skipColor, setSkipColor] = useState("coral")
-  const [skipBackColor, setSkipBackColor] = useState("gray")
-
-  let steps = sequence.length
-  function handleClick() {
-    setCurrIndex((prevStep) => {
-      let newIndex = prevStep + 1;
-      if (newIndex > steps - 1) {
-        newIndex = newIndex - 1
-      } else if (newIndex == steps - 1) {
-        setNextColor("gray")
-        setSkipColor("gray")
-      }
-      if (newIndex > 0) {
-        setBackColor("green")
-        setSkipBackColor("coral")
-      }
-      return newIndex;
-    });
-  }
-
-  function handleClickBack() {
-    setCurrIndex((prevStep) => {
-      let newIndex = prevStep - 1;
-      if (newIndex < 0) {
-        newIndex = 0
-      } else if (newIndex == 0) {
-        setBackColor("gray")
-        setSkipBackColor("gray")
-      }
-      if (newIndex < steps-1) {
-        setNextColor("green")
-        setSkipColor("coral")
-      }
-      return newIndex;
-    });
-  }
-
-  function handleSkipFront() {
-    if (currIndex == sequence.length-1) {
-      return null
-    }
-    if (sequence[currIndex] >= "a3" && sequence[currIndex+1][0] < "g") {
-      setCurrIndex(3+M*3)
-    } else {
-      handleClick()
-    }
-  }
-
-  function handleSkipBack() {
-    if (sequence[currIndex][0] < "g" && sequence[currIndex][0] >= "b") {
-      setCurrIndex(3)
-    } else {
-      handleClickBack()
-    }
-  }
+  // IMPORTANT STATE VARIABLES
+  const [currIndex, setCurrIndex] = useState(0) // State to manage current step
 
   function handleReset() {
     setSubmit2(false)
@@ -154,6 +97,7 @@ export default function Three_SAT_VC({N, M, INPUT, setSubmit, setSubmit2}) {
     edges.push(<Edge key={i+2} index={sequence[currIndex]} id={LETTERS[i/3]+"3"} x1={VAR_SPOTS[i+2]} x2={track+80+CLAUSE_OFFSETS} y2={362}/>)
     track += 154
   }
+
 
   return (
     <main className="flex flex-col items-center justify-between" style={{marginTop:10, marginBottom:118}}>
@@ -220,32 +164,7 @@ export default function Three_SAT_VC({N, M, INPUT, setSubmit, setSubmit2}) {
           </div>
         </div>
       </div>
-      <div className="flex flex-row w-3/12 justify-center items-center p-5" style={{marginTop:180, backgroundColor:"#EAF1F2", height:70, borderRadius:10}}>
-          <div className="flex flex-col items-center justify-center w-1/4">
-            <button onClick={handleSkipBack}>
-                <span style={{color:skipBackColor, fontSize:24}}>&#9664;&#9664;</span>
-            </button>
-            <p style={{fontSize:12}}>skip back</p>
-          </div>
-          <div className="flex flex-col items-center justify-center w-1/4">
-            <button onClick={handleClickBack}>
-              <span style={{color:backColor, fontSize:24}}>&#9664;</span>
-            </button>
-            <p style={{fontSize:12}}>back</p>
-          </div>
-          <div className="flex flex-col items-center justify-center w-1/4">
-            <button onClick={handleClick}>
-              <span style={{color:nextColor, fontSize:24}}>&#9654;</span>
-            </button>
-            <p style={{fontSize:12}}>next</p>
-          </div>
-          <div className="flex flex-col items-center justify-center w-1/4">
-            <button onClick={handleSkipFront}>
-              <span style={{color:skipColor, fontSize:24}}>&#9654;&#9654;</span>
-            </button>
-            <p style={{fontSize:12}}>skip forward</p>
-          </div>
-      </div>
+      <ControlMenu currIndex={currIndex} setCurrIndex={setCurrIndex} sequence={sequence} skipIdx={M}/>
     </main>
   );
 }
