04c2a6979d8295df40734359d23fd964f04eee4b
RF <rinnyfan@umich.edu>
2024-08-14 23:58:03 -1000

    Basic horizontal component for 3SAT->CL

--- src/app/components/sat-to-clique/Arrange.tsx
+++ src/app/components/sat-to-clique/Arrange.tsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react';
 
 // since no need to hide and show, just fill in with data into layout
 // and to make life easier, for the edges, might have a dataset with all the positions of each spot in each layout
-// return appropriate arrangement based on input (horiz, vert, slanted)
+// return appropriate arrangement based on input (horiz, vert, right slant, left slant)
 // ---> a function for each, return a list of components to be rendered W/ POSITIONS
 // ---> use a list to keep track of components for each layout; ex: layout 3: vert, horiz, vert
 
 function Horizontal() {
     return (
-        <svg width="200" height="100">
-            <ellipse cx="100" cy="50" rx="80" ry="40" fill="lightblue" stroke="black" stroke-width="2" />
+        <svg width="300" height="100" x="500" y="250">
+            <ellipse cx="100" cy="50" rx="29" ry="20" fill="lightblue" stroke="black" stroke-width="2" />
+            <ellipse cx="180" cy="50" rx="29" ry="20" fill="lightblue" stroke="black" stroke-width="2" />
+            <ellipse cx="260" cy="50" rx="29" ry="20" fill="lightblue" stroke="black" stroke-width="2" />
         </svg>
     );
 }
