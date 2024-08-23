6ac95fd1b3eef53ab2d40c1f35b7221cd1b58114
RF <rinnyfan@umich.edu>
2024-08-23 18:00:21 -0400

    All sat->cl layouts decided

--- src/app/components/sat-to-clique/Arrange.tsx
+++ src/app/components/sat-to-clique/Arrange.tsx
@@ -26,21 +26,43 @@ function Vertical(x, y) {
     );
 }
 
+function LeftUp(x, y) {
+    return (
+        <svg width="192" height="164" x={x} y={y}>
+            <ellipse cx="32" cy="22" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+            <ellipse cx="64" cy="82" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+            <ellipse cx="96" cy="142" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+        </svg>
+    );
+}
+
+function RightUp(x, y) {
+    return (
+        <svg width="192" height="164" x={x} y={y}>
+            <ellipse cx="96" cy="22" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+            <ellipse cx="64" cy="82" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+            <ellipse cx="32" cy="142" rx="30" ry="20" fill="lightblue" stroke="black" strokeWidth="2" />
+        </svg>
+    );
+}
+
+
 // m = 1: Horizontal(286, 303)
 // m = 2: Horizontal(286, 183), Horizontal(286, 423)
 // m = 3: Vertical(150, 203), Horizontal(286, 423), Vertical(586, 203)
 // m = 4: Vertical(150, 203), Horizontal(286, 123), Vertical(586, 203), Horizontal(286, 423)
-// m = 5: left-right diagonals x2, right-left diagonals x2, horizontal x2
+// m = 5: LeftUp(100, 300), LeftUp(548, 80), RightUp(100, 80), RightUp(548, 300), Horiztonal(286, 493)
 
 
 export default function Arrange() {
 
     return (
     <>
-      {Vertical(150, 203)}
-      {Horizontal(286, 423)}
-      {Vertical(586, 203)}
-      {Horizontal(286, 123)}
+      {LeftUp(100, 300)}
+      {LeftUp(548, 80)}
+      {RightUp(100, 80)}
+      {RightUp(548, 300)}
+      {Horizontal(286, 493)}
     </>
     );
 }
