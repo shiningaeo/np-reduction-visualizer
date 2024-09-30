c757383dd2c6a3d7ee63bab0221ac3e2439abd6b
RF <rinnyfan@umich.edu>
2024-09-29 20:58:56 -0400

    maybe now?

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    /**
-     * Enable static exports for the App Router.
-     *
-     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-     */
-    output: "export",
-  
-    /**
-     * Set base path. This is the slug of your GitHub repository.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-     */
-    basePath: "/nextjs-github-pages",
-  
-    /**
-     * Disable server-based image optimization. Next.js does not support
-     * dynamic features with static exports.
-     *
-     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-     */
-    images: {
-      unoptimized: true,
-    },
-  };
-  
-  export default nextConfig;
+// next.config.js
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  basePath: isProd ? '/np-reduction-visualizer' : '', // GitHub Pages requires base path
+  assetPrefix: isProd ? '/np-reduction-visualizer/' : '',
+  images: {
+    unoptimized: true, // Disable image optimization as it may not work with static export
+  },
+};
